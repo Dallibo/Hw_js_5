081581c3c461e4dccf8d7f381b00d25a1f3b0a5c
Dallibo <denismalyarenko1@gmail.com>
2023-06-06 23:09:45 +0300

    Hw example +

--- Hw_js_5.js
+++ Hw_js_5.js
@@ -6,10 +6,10 @@
 // }
 
 // let numb = +prompt("Введіть число:");
-// let i = numb;
-// while (i >= 0) {
-//     document.write(i + "<br>");
-//     i--;
+// let i1 = numb;
+// while (i1 >= 0) {
+//     document.write(i1 + "<br>");
+//     i1--;
 // }
 
 // let numb2 = +prompt("Введите число:");
@@ -17,17 +17,34 @@
 // let result = Math.pow(numb2, numb3);
 // alert(`Результат: ${result}`);
 
-let numb4 = +prompt("Введите первое число:");
-let numb5 = +prompt("Введите второе число:");
-let min = Math.min(numb4, numb5);
-let del = [];
-let i = 1;
-while (i <= min) {
-  if (numb4 % i === 0 && numb5 % i === 0) {
-    del.push(i);
-  }
-  i++;
-}
-alert ("Общие делители чисел " + numb4 + " и " + numb5 + ": " + del.join(", "));
+// let numb4 = +prompt("Введите первое число:");
+// let numb5 = +prompt("Введите второе число:");
+// let min = Math.min(numb4, numb5);
+// let del = [];
+// let i2 = 1;
+// while (i2 <= min) {
+//   if (numb4 % i2 === 0 && numb5 % i2 === 0) {
+//     del.push(i2);
+//   }
+//   i2++;
+// }
+// alert("Общие делители чисел " + numb4 + " и " + numb5 + ": " + del.join(", "));
+
+
+// let numb6 = +prompt("Введіть число для обчислення факторіалу");
+// let factorial = 1;
+// let i = 1;
+// while (i <= numb6) {
+//     factorial *= i;
+//     i++;
+// }
+// alert("Факторіал числа " + numb6 + " дорівнює " + factorial);
+
+// do {
+//   answer = +prompt("Решите пример: 2 + 2 * 2");
+// } 
+// while (answer !== 6);
+// alert("Правильно! Ответ будет 6.");
+
 
 
