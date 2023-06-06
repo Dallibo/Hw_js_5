c1bcff52fc2203214eb9b48dc233348e35c5498c
Dallibo <denismalyarenko1@gmail.com>
2023-06-07 00:04:32 +0300

    All Hw

--- Hw_js_5.js
+++ Hw_js_5.js
@@ -40,11 +40,61 @@
 // }
 // alert("Факторіал числа " + numb6 + " дорівнює " + factorial);
 
+// let answer;
 // do {
 //   answer = +prompt("Решите пример: 2 + 2 * 2");
 // } 
 // while (answer !== 6);
 // alert("Правильно! Ответ будет 6.");
 
+// let numb7 = +prompt("Скільки разів потрібно поділити 1000 на 2, щоб отримати число менше 50?");
+// let number = 1000;
+// let count2 = 0;
+// do {
+//   number /= 2;
+//   count2++;
+// } while (number >= 50);
+// alert("Отримане число: " + number);
+// alert("Кількість поділів: " + count2);
+
+// let numb10 = +prompt("Введите число");
+// let result = "";
+// for (let i3 = 1; i3 <= 100; i3++) {
+//   if (i3 % numb10 === 0) {
+//     result += i3 + " ";
+//   }
+// }
+// alert("Числа, кратыне " + numb10 + ":\n" + result);
+
+// let numb11 = +prompt("Введіть ваше максимальне число:");
+// let numb12 = +prompt("Введіть ваше мінімальне число:");
+// let result2 = "";
+// for (let i = numb12; i <= numb11; i += 4) {
+//   result2 += i + " ";
+// }
+// alert("Кожен 4-ий елемент в діапазоні від " + numb12 + " до " + numb11 + ":\n" + result2);
+
+let numb13 = +prompt("Введите число:");
+let numb14 = true;
+if (numb13 === 1) {
+  numb14 = false;
+} 
+else if (numb13 > 1) {
+  for (let i = 2; i < numb13; i++) {
+    if (numb13 % i === 0) {
+      numb14 = false;
+      break;
+    }
+  }
+} 
+else {
+  numb14 = false;
+}
+
+if (numb14) {
+  alert(numb13 + " это простое число.");
+} else {
+  alert(numb13 + "это не простое число.");
+}
 
 
